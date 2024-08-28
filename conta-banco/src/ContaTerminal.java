f07f5beef19926429644842d9e354918780df3e5
spokbg <131629032+spokbg@users.noreply.github.com>
2024-08-27 21:16:06 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      public static void main(String[] args) throws Exception {
+        
+        String usuario = "MARIO ANDRADE";
+        Double saldo = 237.48;
+
+
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("Por favor digite o numero da agência!");
+          String agencia = scanner.next();
+
+         System.out.println("Por favor digite o numero da conta!");
+          int numeroConta = scanner.nextInt();
+
+         System.out.println("Olá " + usuario + "! Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " Conta " + numeroConta + " e seu saldo de " + saldo + " Reais já está disponível para saque!");       
+
+
+
+
+        
     }
 }
