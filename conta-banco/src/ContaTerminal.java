53f4ce028a50162732f56dbde22de3471297c1ac
andremdm <bglink@hotmail.com>
2024-08-27 23:32:19 -0300

    implmentando metodo concat

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -16,8 +16,8 @@ public class ContaTerminal {
          System.out.println("Por favor digite o numero da conta:(1021)");
           int numeroConta = scanner.nextInt();
 
-         System.out.println("Olá " + nomeCliente +
-           "! Obrigado por criar uma conta em nosso banco, agência " + agencia +
+         System.out.println("Olá ".concat(nomeCliente) + 
+           "! Obrigado por criar uma conta em nosso banco, agência ".concat(agencia) + 
            " conta nº " + numeroConta + " e seu saldo de " + saldo + " Reais já está disponível para saque!");
            
             
