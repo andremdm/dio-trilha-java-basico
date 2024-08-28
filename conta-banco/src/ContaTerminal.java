2d341eb6efb83ff8b875691a6d4ee62704e6be0d
andremdm <bglink@hotmail.com>
2024-08-28 14:07:26 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class ContaTerminal {
       public static void main(String[] args) throws Exception {
          
-        Double saldo = 237.48;
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Por favor digite seu Nome:");
@@ -15,6 +13,9 @@ public class ContaTerminal {
 
          System.out.println("Por favor digite o numero da conta:(1021)");
           int numeroConta = scanner.nextInt();
+
+         System.out.println("Por favor digite o saldo da conta:");
+          Double saldo = scanner.nextDouble();
 
          System.out.println("Olá ".concat(nomeCliente) + 
            "! Obrigado por criar uma conta em nosso banco, agência ".concat(agencia) + 
